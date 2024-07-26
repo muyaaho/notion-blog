88c37a911633132b49d706fa0fda390a3251efed
muyaaho <skanthsu715@gmail.com>
2024-07-26 19:14:06 +0900

    feat: ga script

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -19,14 +19,29 @@ class MyDocument extends Document {
             href="/feed"
           ></link>
           {/* <!-- Google tag (gtag.js) --> */}
-          <script async src="https://www.googletagmanager.com/gtag/js?id=G-Q1TXY13P5Y"></script>
-          <script>
-            window.dataLayer = window.dataLayer || [];
+          <Script async src={`https://www.googletagmanager.com/gtag/js?id=G-Q1TXY13P5Y`}></Script>
+        {/* <Script
+          src={`https://www.googletagmanager.com/gtag/js?id=${CONFIG.googleAnalytics.config.measurementId}`}
+        /> */}
+        {/* <Script strategy="lazyOnload" id="ga">
+          {`window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
+            gtag('config', '${CONFIG.googleAnalytics.config.measurementId}', {
+              page_path: window.location.pathname,
+            });`}
+        </Script> */}
+        <Script strategy="lazyOnload" id="ga"> 
+          {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
 
-            gtag('config', 'G-Q1TXY13P5Y');
-          </script>
+          gtag('config', 'G-Q1TXY13P5Y');
+          
+          `}
+          
+          </Script>
           {/* google search console */}
           {CONFIG.googleSearchConsole.enable === true && (
             <>
